350b5f457c048c6a7647a9064a1334628ec9b018
Jerry Hsia <xiajie9916@gmail.com>
2014-10-29 09:10:57 +0800

    Updated code

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,24 +16,54 @@ module.exports = function (grunt) {
     },
 
     concat: {
-      options: {
-        separator: ';'
-      },
-      blog: {
+      blogCss: {
         files: {
-          '_temp/styles/base.css': [
+          '_temp/blog/styles/base.css': [
             'assets/styles/font-awesome.css',
             'assets/styles/main.css',
             'assets/styles/page.css'
+          ]
+        }
+      },
+      resumeCss: {
+        options: {
+          separator: ''
+        },
+        files: {
+          '_temp/resume/styles/base.css': [
+            'resume/styles/bootstrap.css',
+            'resume/styles/font-awesome.css',
+            'resume/styles/style.css'
+          ]
+        }
+      },
+      resumeScript: {
+        options: {
+          separator: ';'
+        },
+        files: {
+          '_temp/resume/scripts/base.js': [
+            'resume/styles/bootstrap.css',
+            'resume/styles/font-awesome.css',
+            'resume/styles/style.css'
           ]
         }
       }
     },
 
     cssmin: {
+      options: {
+        banner: '/* Jerry\'s blog */',
+        keepSpecialComments: 0
+      },
       blog: {
         files: {
-          'assets/styles/base.min.css': ['_temp/styles/base.css']
+          'assets/styles/base.min.css': ['_temp/blog/styles/base.css']
+        }
+      },
+      resume: {
+        files: {
+          'resume/styles/base.min.css': ['_temp/resume/styles/base.css']
         }
       }
     },
@@ -58,7 +88,8 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('blog', ['clean:temp', 'concat:blog', 'cssmin:blog', 'clean:temp']);
+  grunt.registerTask('blog', ['clean:temp', 'concat:blogCss', 'cssmin:blog', 'clean:temp']);
+  grunt.registerTask('resume', ['clean:temp', 'concat:resumeCss', 'concat:resumeScript', 'cssmin:resume', 'clean:temp']);
 
   grunt.registerTask('publish', function() {
     return grunt.task.run(['blog', 'shell:jekyllBuild', 'githubPages']);
